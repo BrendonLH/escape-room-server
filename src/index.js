c04ccd66c6c351da9548abeee83e200bf465f856
Brend <brendonhampton1988@gmail.com>
2021-12-19 12:13:59 -0800

    fixed post crud, saving players to db works now

--- src/index.js
+++ src/index.js
@@ -4,43 +4,56 @@ dotenv.config();
 const {MongoClient} = require('mongodb');
 const cors = require('cors');
 const app = express();
-// const riddles = require('./riddles.js');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use(cors());
 
 // get DB info
 let riddlesArr;
 
-async function main() {
-    const uri = process.env.MONGODB_URI;
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    await client.connect();
-    findListings(client);
-  }
+async function getRiddles() {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  await client.connect();
+  findListings(client);
+}
+async function findListings(client) {
+  const cursor = client
+    .db('escape_room')
+    .collection('riddles')
+    .find()
 
-  main().catch(console.error);
+  const results = await cursor.toArray();
+  riddlesArr = results;
+}
 
-  async function findListings(client) {
-    const cursor = client
-      .db('escape_room')
-      .collection('riddles')
-      .find()
-  
-    const results = await cursor.toArray();
-    riddlesArr = results;
-    console.log(riddlesArr);
-  }
+async function postPlayer(player) {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  await client.connect();
+  client.db('escape_room').collection('players').insertOne(player, (error,data) => {
+      if(error) return console.log(error);
+    })
+}
+
+getRiddles().catch(console.error);
+
+
 
 
 // get riddles
 app.get('/riddles',(request,response) => {
-    response.json({ message: {riddlesArr} });
+  response.json({ message: {riddlesArr} });
 });
-
 // post route to DB
+app.post('/post',(request,response) => {
+  postPlayer(request.body);
+});
 
 // get route for top scores
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('server up and running');
 });
